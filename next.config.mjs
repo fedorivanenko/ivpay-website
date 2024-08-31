5fdfdc7d142181ac70eb0725a1afbd1c26939528
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2024-08-31 15:43:56 +0500

    remove three.js and globe.gl from SSR build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 import withPlaiceholder from "@plaiceholder/next";
 import withBundleAnalyzer from '@next/bundle-analyzer';
+//import nextTranslate from "next-translate";
+import nextComposePlugins from "next-compose-plugins";
+
+const { withPlugins } = nextComposePlugins;
 
 const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
@@ -9,7 +13,24 @@ const bundleAnalyzer = withBundleAnalyzer({
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  webpack(config) {
+  webpack: (config, { isServer }) => {
+    // Optimize Three.js
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'three$': 'three/src/Three.js',
+    };
+
+    // Exclude Three.js from server-side rendering
+    if (isServer) {
+      config.externals = [
+        ...config.externals,
+        'three',
+        'globe.gl',
+        'react-globe.gl'
+      ];
+    }
+
+    // SVG handling
     config.module.rules.push({
       test: /\.svg$/,
       use: [
@@ -24,6 +45,19 @@ const nextConfig = {
 
     return config;
   },
+  productionBrowserSourceMaps: process.env.ANALYZE === 'true',
+  reactStrictMode: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    //domains: ["***"], // Replace with your actual domains
+  },
 };
 
-export default bundleAnalyzer(withPlaiceholder(nextConfig));
+export default withPlugins(
+  [
+    withPlaiceholder,
+    bundleAnalyzer,
+    //nextTranslate,
+  ],
+  nextConfig
+);
