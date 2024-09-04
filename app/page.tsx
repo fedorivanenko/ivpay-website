d39293f889b2be788ee194b59e3a3277583ba914
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2024-09-04 20:25:13 +0500

    final home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import CoinsListBlock from "@/components/blocks/coins-list-block";
 import EasySafeFastBlock from "@/components/blocks/easy-safe-fast-block";
 import HomeHeroBlock from "@/components/blocks/home-hero-block";
-import MerchantVideoBlock from "@/components/blocks/merchants-video-block";
+//import MerchantVideoBlock from "@/components/blocks/merchants-video-block";
 import OurProductsBlock from "@/components/blocks/our-products-block";
 import ReapTheBenfitsBlock from "@/components/blocks/reap-the-benefits-block";
 import TogetherWithUsBlock from "@/components/blocks/together-with-us-block";
@@ -9,12 +9,13 @@ import TogetherWithUsBlock from "@/components/blocks/together-with-us-block";
 export default function Home() {
   return (
     <main className="flex flex-col">
+      {/*
+      */}
       <HomeHeroBlock/>
       <OurProductsBlock/>
       <EasySafeFastBlock/>
       <ReapTheBenfitsBlock/>
       <CoinsListBlock/>
-      <MerchantVideoBlock/>
       <TogetherWithUsBlock/>
     </main>
   );
