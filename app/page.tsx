628742a663dfa83add3a7fd84d2c078a958f8d88
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2024-09-05 19:26:59 +0500

    fix imports at home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,23 @@
-import CoinsListBlock from "@/components/blocks/coins-list-block";
-import EasySafeFastBlock from "@/components/blocks/easy-safe-fast-block";
+import CoinsListBlock from "@/components/blocks/home-coins-list-block";
+import EasySafeFastBlock from "@/components/blocks/home-easy-safe-fast-block";
 import HomeHeroBlock from "@/components/blocks/home-hero-block";
 //import MerchantVideoBlock from "@/components/blocks/merchants-video-block";
-import OurProductsBlock from "@/components/blocks/our-products-block";
+import OurProductsBlock from "@/components/blocks/home-our-products-block";
 import ReapTheBenfitsBlock from "@/components/blocks/reap-the-benefits-block";
-import TogetherWithUsBlock from "@/components/blocks/together-with-us-block";
+import TogetherWithUsBlock from "@/components/blocks/home-together-with-us-block";
 
 
 export default function Home() {
   return (
     <main className="flex flex-col">
-      {/*
-      */}
+      <EasySafeFastBlock/>
       <HomeHeroBlock/>
       <OurProductsBlock/>
-      <EasySafeFastBlock/>
       <ReapTheBenfitsBlock/>
       <CoinsListBlock/>
       <TogetherWithUsBlock/>
+      {/*
+      */}
     </main>
   );
 }
