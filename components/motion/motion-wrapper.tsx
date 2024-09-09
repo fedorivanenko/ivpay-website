63152bf5765ede07ecb210e4fa58203ad78ad00b
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2024-09-09 15:55:14 +0500

    fix MotionWrapper

--- components/motion/motion-wrapper.tsx
+++ components/motion/motion-wrapper.tsx
@@ -1,66 +1,38 @@
 "use client";
 
 import * as React from "react";
-import { m, MotionProps } from "framer-motion";
-import { contentAppearing } from "@/components/motion/motion_utils";
+import * as Slot from '@radix-ui/react-slot';
+import { m, MotionProps, Variants } from 'framer-motion';
+import { contentAppearing } from "./motion_utils"; 
 
-type MotionWrapperProps = MotionProps & {
-  children: React.ReactNode;
-  className?: string;
-  asDiv?: boolean;
-};
+type MotionWrapperProps = React.HTMLAttributes<HTMLElement> &
+  MotionProps & {
+    asDiv?: boolean;
+    variants?: Variants;
+  };
 
 /**
- * Wraps a single child element with Framer Motion capabilities.
+ * Wraps a single child element with Framer Motion capabilities with radix-ui/react-slot under the hood.
  *
- * <MotionWrapper animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+ * Usage example:
+ * ```jsx
+ * <MotionSlot animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
  *   <YourComponent />
- * </MotionWrapper>
- *
- * Note: The child must be a valid React element. Strings, numbers, or other non-element nodes will cause an error.
+ * </MotionSlot>
+ * ```
  */
 
-const MotionWrapper = React.forwardRef<unknown, MotionWrapperProps>(
-  (
-    {
-      children,
-      className,
-      variants = contentAppearing,
-      asDiv = false,
-      ...motionProps
-    },
-    ref,
-  ) => {
-    if (!asDiv) {
-      React.Children.only(children);
-      if (React.isValidElement(children)) {
-        const MotionComponent = React.useMemo(() => m(children.type), [children.type]);
-        return (
-          <MotionComponent
-            variants={variants}
-            {...children.props}
-            {...motionProps}
-            ref={ref as React.ForwardedRef<Element>}
-          />
-        );
-      }
-      throw new Error(
-        "MotionWrapper only accepts a single React element as a child",
-      );
-    } else {
-      return (
-        <m.div
-          className={className}
-          variants={variants}
-          {...motionProps}
-          ref={ref as React.ForwardedRef<HTMLDivElement>}
-        >
-          {children}
-        </m.div>
-      );
-    }
-  },
+const MotionWrapper = React.forwardRef<HTMLElement, MotionWrapperProps>(
+  ({ asDiv = false, variants = contentAppearing, children, ...props }, ref) => {
+    const Comp = !asDiv ? Slot.Slot : 'div';
+    const MotionComponent = React.useMemo(() => m(Comp as any), [Comp]);
+    return (
+      <MotionComponent ref={ref} {...props} variants={variants}>
+        {children}
+      </MotionComponent>
+    );
+  }
 );
-MotionWrapper.displayName = "MotionWrapper";
+MotionWrapper.displayName = "MotionSlot";
 
 export { MotionWrapper };
