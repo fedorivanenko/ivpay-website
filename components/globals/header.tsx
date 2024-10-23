f94220349dd038842991ef320a71f3df740933a8
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2024-10-23 13:52:31 +0500

    change inner links from ivendpay → ivpay

--- components/globals/header.tsx
+++ components/globals/header.tsx
@@ -46,12 +46,12 @@ const Header = React.forwardRef<
             <div className="pointer-events-auto flex justify-end space-x-2 lg:w-60 ">
                 <Button asChild variant="white">
                   {/*//TODO: MAKE IT VAR */}
-              <Link href="https://app.ivendpay.com/login">
+              <Link href="https://app.ivpay.io/login">
                   Log In
               </Link>
                   </Button>
                 <Button asChild>
-              <Link href="https://app.ivendpay.com/register">
+              <Link href="https://app.ivpay.io/register">
                   Sign Up
               </Link>
                   </Button>
